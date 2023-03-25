9e2ae4d15856d05beb740fbb5a047310a8dd218f
calel95 <borgescalel@gmail.com>
2023-03-25 00:10:58 -0300

    adicionado tratamento de perder o jogo, os erros quando usuário cometer x erros, e criado um arquivo

--- forca.py
+++ forca.py
@@ -4,15 +4,16 @@ def jogar():
     palavra_secreta = 'banana'.upper()
     palavra_secreta2 = list(palavra_secreta)
     perdeu = False
+    erros = 0
     ganhou = False
     count = 0
     letras_certas = []
 
-    print(palavra_secreta2)
+    #print(palavra_secreta2)
 
 
     for i in range(0,len(palavra_secreta)):
-        print(i)
+        #print(i)
         letras_certas.append("_")
     print(letras_certas)
 
@@ -22,25 +23,24 @@ def jogar():
 
         letras = input("Digite uma letra: ").upper().strip()
 
-
-        for index, letra in enumerate(palavra_secreta):
-            if letra == letras:
-                print(index, ':', letra)
-                letras_certas[index] = letra
+        if letras in palavra_secreta:
+            for index, letra in enumerate(palavra_secreta):
+                if letra == letras:
+                    print(index, ':', letra)
+                    letras_certas[index] = letra
+            if letras_certas == palavra_secreta2:
+                ganhou = True
+                print("Você ganhoooouu, PArabéns!!")
+        else:
+            erros = erros + 1
+            palavra_secreta
+            print(f"Erros {erros}/7")
+            if erros == 7:
+                perdeu = True
+                print(f"Você perdeu, a palavra secreta era {palavra_secreta}")
+            else:
+                continue
         print(letras_certas)
-        if letras_certas == palavra_secreta2:
-            ganhou = True
-            print("Você ganhoooouu, PArabéns!!")
-
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
