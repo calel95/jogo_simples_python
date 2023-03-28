0f72be339ca961e9036b0a8b7dd0a75e99ba3609
calel95 <borgescalel@gmail.com>
2023-03-27 23:50:06 -0300

    Adicionado funções para organizar o jogo, e adicionado o desenho da forca, e adiciona as letras erradas

--- forca.py
+++ forca.py
@@ -2,17 +2,7 @@ import random
 def jogar():
     print("FORCA")
 
-    def escolha_palavra():
-        #FAZ A ABERTURA DO ARQUIVO, ONDE ESTÁ AS PALAVRAS SECRETAS, E ESCOLHE UMA ALEATÓRIAMENTE E SELECIONA NO JOGO
-        #arq = open("palavras.txt", "r", encoding="utf-8")
-        with open("palavras.txt", encoding="utf-8") as arq:
-            palavras_arquivo = []
-            for i in arq:
-                palavras_arquivo.append(i.replace("\n","").upper())
-                print(i)
-        print(palavras_arquivo)
-        palavra_chave = ''.join(random.choices(palavras_arquivo))  #join passa a palavra da lista extraida pra string, unindo todas as palavras em uma
-        return palavra_chave
+
 
     #print(palavra_secreta)
     #DEFINE AS VARIÁVEIS, E RECRIA A PALAVRA SECRETA, PARA COMPARAR COM A ORIGINAL NO FINAL
@@ -24,6 +14,7 @@ def jogar():
     ganhou = False
     count = 0
     letras_certas = []
+    letras_erradas = []
 
 
     #CRIA O CAMPO DA PALAVRA CONFORME O TAMANHO DELA PARA MOSTRAR NA TELA
@@ -35,7 +26,7 @@ def jogar():
     # enquanto não enfoucou e não acertou
     #ABRE O LAÇO DE REPITIÇÃO, ENQUANTO O STATUS DE PERDEU OU GANHOU NÃO ALTERAR, O JOGO SE REPETE
     while not perdeu and not ganhou:
-        print("Jogo em andamento...")
+        print("Jogo em andamento...\n")
 
         letras = input("Digite uma letra: ").upper().strip()
 
@@ -43,7 +34,7 @@ def jogar():
         if letras in palavra_secreta:
             for index, letra in enumerate(palavra_secreta):
                 if letra == letras:
-                    print(index, ':', letra)
+                    #print(index, ':', letra)
                     letras_certas[index] = letra
             #QUANDO A PALAVRA SECRETA ESTIVER PREENCHIDA, ELA VAI FICAR IGUAL A CÓPIA CRIADA NO INICIO, ASSIM MUDA O STATUS DE GANHOU PRA TRUE E ELE VENCE!
             if letras_certas == palavra_secreta2:
@@ -53,14 +44,88 @@ def jogar():
         else:
             erros = erros + 1
             palavra_secreta
-            print(f"Erros {erros}/7")
+            #print(f"Erros {erros}/7")
+            desenha_forca(erros,letras_certas,letras_erradas)
+            letras_erradas.append(letras)
             if erros == 7:
                 perdeu = True
-                print(f"Você perdeu, a palavra secreta era {palavra_secreta}")
+                print(f"Que pena, Você perdeu!! A palavra secreta era {palavra_secreta}")
             else:
                 continue
         print(letras_certas)
+        print("\nLetras digitadas erradas:", letras_erradas)
 
+def desenha_forca(erros,letras_certas,letras_erradas):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if(erros == 1):
+        print(" |      (=)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+
+    if(erros == 2):
+        print(" |      (_)   ")
+        print(" |       |    ")
+        print(" |            ")
+        print(" |            ")
+
+
+    if(erros == 3):
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+
+
+    if(erros == 4):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+
+    if(erros == 5):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+
+    if(erros == 6):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+
+    if (erros == 7):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
+
+    print(letras_certas)
+    print("Letras Erradas:", letras_erradas)
+
+def escolha_palavra():
+    #FAZ A ABERTURA DO ARQUIVO, ONDE ESTÁ AS PALAVRAS SECRETAS, E ESCOLHE UMA ALEATÓRIAMENTE E SELECIONA NO JOGO
+    #arq = open("palavras.txt", "r", encoding="utf-8")
+    with open("palavras.txt", encoding="utf-8") as arq:
+        palavras_arquivo = []
+        for i in arq:
+            palavras_arquivo.append(i.replace("\n","").upper())
+            #print(i)
+    #print(palavras_arquivo)
+    palavra_chave = ''.join(random.choices(palavras_arquivo))  #join passa a palavra da lista extraida pra string, unindo todas as palavras em uma
+    return palavra_chave
 
 if __name__ == "__main__":
     jogar()
